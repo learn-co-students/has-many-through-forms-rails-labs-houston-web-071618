12170850f239ca954f51fd4ee4b9a8e0f7ad6772
github username <github email address>
2018-08-19 20:37:25 -0500

    Done.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,16 @@
 class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :post
+  accepts_nested_attributes_for :user, :reject_if => :missing_both_user_id_and_username
+
+  def missing_user_id_or_username(attributes)
+    attributes[:user_id].nil? && attributes[:username].nil?
+  end
+
+  def user_attributes=(user_attributes)
+    if user_attributes[:username] != ""
+      self.user = User.create(username: user_attributes[:username])
+    end
+  end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+first_user = User.create(username: "Crate", email: "crate@create.com")
+second_user = User.create(username: "The humblest", email: "hh@h.com")
+third_user = User.create(username: "Everyone's Boss", email: "theboss@yourcompany.com")
+
+first_post = Post.create(title: "First!", content: "This is the first post ever and this is real content content content content content content content content content content content content content content content content content content content content content content content content content content content content content content content content content content content content content content content content content content content content content content content content content content content content content content content content content content content content content content content content content content content content content content content content content content content.... CONTENT.")
+second_post = Post.create(title: "Not the pretentious one.", content: "Thank you for reading. I have nothing else to say.")
+third_post = Post.create(title: "THIS IS A TITLE", content: "I HAVE BEEN TOLD THAT IT IS INAPPROPRIATE TO TYPE IN ALL CAPS, AS IT SEEMS LIKE I AM SCREAMING, BUT I DON'T KNOW HOW TO TURN THIS OFF. ALSO, I AM HARD OF HEARING, SO WHAT DOES IT REALLY MATTER? IS THIS FORMAT NOT EASIER TO READ ANYWAYS?")
+
+new_cat = Category.create(name: "new")
+old_cat = Category.create(name: "old")
+cool_cat = Category.create(name: "cool")
